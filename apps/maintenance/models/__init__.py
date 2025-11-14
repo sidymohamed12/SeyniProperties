b86297d5955c1c2f071bbebc49f5231b34605e29
Sidy <mohamedsaizonou86@gmail.com>
2025-11-14 08:18:58 +0000

    le init du model

---
+++ apps/maintenance/models/__init__.py
@@ -0,0 +1,18 @@
+
+from .travail import Travail, TravailChecklist, TravailMedia
+from .tache import Tache
+from .intervention import Intervention, InterventionChecklistItem, InterventionMedia, InterventionTemplate, InterventionTemplateChecklistItem
+from .maintenance import MaintenanceSchedule
+
+__all__ = [
+    'Travail',
+    'TravailChecklist',
+    'TravailMedia',
+    'Tache',
+    'Intervention',
+    'InterventionChecklistItem',
+    'InterventionMedia',
+    'InterventionTemplate',
+    'InterventionTemplateChecklistItem',
+    'MaintenanceSchedule',
+]
